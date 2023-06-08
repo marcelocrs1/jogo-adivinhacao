ddf6853415f82f3d0f707e39ab7da7b876e093b5
Marcelo Santos <marcelocrs@gmail.com>
2023-06-08 23:53:57 +0100

    Aula inicial

--- adivinhacao.py
+++ adivinhacao.py
@@ -1,1 +1,16 @@
+print("******************************")
 print("Bem vindo ao jogo Adivinhação!")
+print("******************************")
+
+numero_secreto = 42
+
+chute_str = input("Digite o seu número: ")
+
+chute = int(chute_str)
+
+print("Você digitou ", chute)
+
+if(numero_secreto == chute):
+    print("Você acertou!")
+else:
+    print("Você errou!")
