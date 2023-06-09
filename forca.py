8fd002a809e1109a81ac875d811382edc493e3ec
Marcelo Santos <marcelocrs@gmail.com>
2023-06-09 20:04:09 +0100

    Organizando em funções

--- forca.py
+++ forca.py
@@ -1,10 +1,11 @@
-def jogar():
-    print("******************************")
-    print("Bem vindo ao jogo da Forca!")
-    print("******************************")
+import random
 
-    palavra_secreta = "python".upper()
-    letras_acertadas = ["_" for letra in palavra_secreta]
+
+def jogar():
+
+    text_open_game()
+    palavra_secreta = load_secret_word()
+    letras_acertadas = start_words_rights(palavra_secreta)
 
     # for letra in palavra_secreta:
     #     letras_acertadas.append("_")
@@ -40,6 +41,31 @@ def jogar():
 
     print("Fim do Jogo")
 
+
+# ********* Texto Abertura *********
+def text_open_game():
+    print("******************************")
+    print("Bem vindo ao jogo da Forca!")
+    print("******************************")
+
+# ********* Função Carregar Palavra Secreta *********
+def load_secret_word():
+    arquivo = open("palavras.txt", "r")
+    palavras = []
+
+    for linha in arquivo:
+        linha = linha.strip()
+        palavras.append(linha)
+
+    arquivo.close()
+
+    numero = random.randrange(0, len(palavras))
+    palavra_secreta = palavras[numero].upper()
+    return palavra_secreta
+
+# ********* Função Inicializar Palavra Secreta *********
+def start_words_rights(palavra):
+    return ["_" for letra in palavra]
 
 if(__name__ == "__main__"):
     jogar()
