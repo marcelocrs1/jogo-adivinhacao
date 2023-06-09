0c3212da8cc4a8d741f4490a80a93133ae8a142d
Marcelo Santos <marcelocrs@gmail.com>
2023-06-09 16:44:59 +0100

    Game forca in construction

--- forca.py
+++ forca.py
@@ -1,8 +1,28 @@
-
 def jogar():
     print("******************************")
     print("Bem vindo ao jogo da Forca!")
     print("******************************")
+
+    palavra_secreta = "python"
+    letras_acertadas = ["_", "_", "_", "_", "_", "_",]
+
+    print(letras_acertadas)
+
+    enforcou = False
+    acertou = False
+
+    while(not enforcou and not acertou):
+
+        chute = input("Digite uma letra: ")
+        chute = chute.strip()
+
+        index = 0
+        for letra in palavra_secreta:
+            if(chute.upper() == letra.upper()):
+                letras_acertadas[index] = letra
+            index = index + 1
+
+        print(letras_acertadas)
 
     print("Fim do Jogo")
 
