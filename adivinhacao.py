d0fc1723fbe0d33029f668dbb50f98095fb11d52
Marcelo Santos <marcelocrs@gmail.com>
2023-06-09 12:02:13 +0100

    Round game logics created

--- adivinhacao.py
+++ adivinhacao.py
@@ -3,25 +3,29 @@ print("Bem vindo ao jogo Adivinhação!")
 print("******************************")
 
 numero_secreto = 42
+total_de_tentativas = 3
+rodada = 1
 
-chute_str = input("Digite o seu número: ")
+while (rodada <= total_de_tentativas):
+    print("Tentativa", rodada, "de ", total_de_tentativas)
+    chute_str = input("Digite o seu número: ")
+    chute = int(chute_str)
 
-chute = int(chute_str)
+    acertou = chute == numero_secreto
+    maior = chute > numero_secreto
+    menor = chute < numero_secreto
 
-acertou = chute == numero_secreto
+    print("Você digitou ", chute)
 
-maior = chute > numero_secreto
+    if(acertou):
+      print("Você acertou!")
+    else:
+        if(maior):
+            print("Você errou! O seu chute foi maior do que o número secreto!")
+        elif(menor):
+            print("Você errou! O seu chute foi menor do que o número secreto!")
 
-menor = chute < numero_secreto
 
-print("Você digitou ", chute)
-
-if(acertou):
-    print("Você acertou!")
-else:
-    if(maior):
-        print("Você errou! O seu chute foi maior do que o número secreto!")
-    elif(menor):
-        print("Você errou! O seu chute foi menor do que o número secreto!")
+    rodada += 1
 
 print("Fim de jogo")
