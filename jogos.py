bf948ca4cbd53d6ac4d014eff8ddb4bae407b6e7
Marcelo Santos <marcelocrs@gmail.com>
2023-06-09 15:24:13 +0100

    First Course finished

--- jogos.py
+++ jogos.py
@@ -2,20 +2,20 @@ import  forca
 import  adivinhacao
 
 def escolha_jogo():
-print("******************************")
-print("******Escolha o seu jogo******")
-print("******************************")
+    print("******************************")
+    print("******Escolha o seu jogo******")
+    print("******************************")
 
-print("(1) Forca (2) Adivinhação")
+    print("(1) Forca (2) Adivinhação")
 
-jogo = int(input("Qual jogo?"))
+    jogo = int(input("Qual jogo?"))
 
-if(jogo == 1):
-    print("Jogo da Forca selecionado")
-    forca.jogar()
-elif(jogo == 2):
-    print("Jogo da Adivinhação selecionado")
-    adivinhacao.jogar()
+    if(jogo == 1):
+        print("Jogo da Forca selecionado")
+        forca.jogar()
+    elif(jogo == 2):
+        print("Jogo da Adivinhação selecionado")
+        adivinhacao.jogar()
 
-if(__name__ == "__main__"):
-    escolha_jogo()
+    if(__name__ == "__main__"):
+        escolha_jogo()
