54a3fd21fdccf82764a0ec95d7216043c7e5f264
Marcelo Santos <marcelocrs@gmail.com>
2023-06-09 14:43:25 +0100

    Next game created

--- adivinhacao.py
+++ adivinhacao.py
@@ -6,8 +6,20 @@ print("******************************")
 
 numero_random = random.randrange(1, 101)
 numero_secreto = numero_random
-total_de_tentativas = 3
-rodada = 1
+total_de_tentativas = 0
+pontos = 1000
+
+print("Qual o nível de dificuldade?")
+print("(1) Fácil (2) Médio (3) Difícil")
+
+nivel = int(input("Defina o nivel: "))
+
+if(nivel == 1):
+    total_de_tentativas = 10
+elif(nivel == 2):
+    total_de_tentativas = 5
+else:
+    total_de_tentativas = 3
 
 for rodada in range(1, total_de_tentativas + 1):
     print("Tentativa {} de {}".format(rodada, total_de_tentativas))
@@ -25,17 +37,21 @@ for rodada in range(1, total_de_tentativas + 1):
     print("Você digitou ", chute)
 
     if (acertou):
-        print("Você acertou!")
+        print("Você acertou e fez {} pontos!".format(pontos))
         break
     else:
         if (maior):
             print("Você errou! O seu chute foi maior do que o número secreto!")
         elif (menor):
             print("Você errou! O seu chute foi menor do que o número secreto!")
+        pontos_perdidos = abs(numero_random - chute)
+        pontos = pontos - pontos_perdidos
 
 print("Fim de jogo o numero secreto era:", numero_secreto)
 
 # ********** Usando o Loop While inves do for **********
+
+# rodada = 1
 
 # while (rodada <= total_de_tentativas):
 #     print("Tentativa {} de {}".format(rodada, total_de_tentativas))
