6dc496e700a646c84d891f81d9a83f1dd0cc8ba3
Marcelo Santos <marcelocrs@gmail.com>
2023-06-09 12:43:25 +0100

    Change while to for

--- adivinhacao.py
+++ adivinhacao.py
@@ -6,7 +6,28 @@ numero_secreto = 42
 total_de_tentativas = 3
 rodada = 1
 
-while (rodada <= total_de_tentativas):
+# while (rodada <= total_de_tentativas):
+#     print("Tentativa {} de {}".format(rodada, total_de_tentativas))
+#     chute_str = input("Digite o seu número: ")
+#     chute = int(chute_str)
+#
+#     acertou = chute == numero_secreto
+#     maior = chute > numero_secreto
+#     menor = chute < numero_secreto
+#
+#     print("Você digitou ", chute)
+#
+#     if (acertou):
+#         print("Você acertou!")
+#     else:
+#         if (maior):
+#             print("Você errou! O seu chute foi maior do que o número secreto!")
+#         elif (menor):
+#             print("Você errou! O seu chute foi menor do que o número secreto!")
+#
+#     rodada += 1
+
+for rodada in range(1, total_de_tentativas + 1):
     print("Tentativa {} de {}".format(rodada, total_de_tentativas))
     chute_str = input("Digite o seu número: ")
     chute = int(chute_str)
@@ -17,15 +38,14 @@ while (rodada <= total_de_tentativas):
 
     print("Você digitou ", chute)
 
-    if(acertou):
-      print("Você acertou!")
+    if (acertou):
+        print("Você acertou!")
     else:
-        if(maior):
+        if (maior):
             print("Você errou! O seu chute foi maior do que o número secreto!")
-        elif(menor):
+        elif (menor):
             print("Você errou! O seu chute foi menor do que o número secreto!")
 
 
-    rodada += 1
 
 print("Fim de jogo")
